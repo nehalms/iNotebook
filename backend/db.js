dbc2362cadf1e2b6a5dea1513c4a96cc466d6ca2
nehalms <116992269+nehalms@users.noreply.github.com>
2024-07-29 15:47:58 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 // const mongoURI = "mongodb://127.0.0.1/inotebook" //inotebook is database name
-
+const password = encodeURIComponent("nehal#02");
 const connectToMongo = ()=> {
-    mongoose.connect(process.env.REACT_APP_MONGO_URL).then(console.log("Connected to Mongodb"));
+    mongoose.connect(`mongodb+srv://nehms1982:${password}@cluster0.ifjjiuu.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0`).then(console.log("Connected to Mongodb"));
 }
+
 
 module.exports = connectToMongo;
