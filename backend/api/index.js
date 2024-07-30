1de800f9d8955962ac05c59d21f8e1fe6dc68833
nehalms <116992269+nehalms@users.noreply.github.com>
2024-07-30 13:31:46 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,6 +14,10 @@ let corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 
+app.get("/", (res, req) => {
+ res.send("Hello");
+});
+
 //Available routes
 app.use('/api/auth', require('../routes/auth'));
 app.use('/api/notes', require('../routes/notes'));
