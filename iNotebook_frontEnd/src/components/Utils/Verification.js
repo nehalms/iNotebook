b85ad4ea74098c64076342207510e535b494ea26
nehalms <nehalms3388@gmail.com>
2024-10-29 16:11:11 +0530

    otp module changes

--- iNotebook_frontEnd/src/components/Utils/Verification.js
+++ iNotebook_frontEnd/src/components/Utils/Verification.js
@@ -1,52 +1,59 @@
 import { React, useState }  from 'react'
-import './Verification.css'
+// import './Verification.css'
 
 function Verification(props) {
-  const[code, setCode] = useState({code : 0});
+  const [otp, setOtp] = useState(new Array(6).fill(""));
   
-  const onChange = (e)=> {
-    // setCode({[e.target.name]: code.code + e.target.value}) //helps to keep data in note as same and append the new values being typed
-    let num = 6;
-    let tmpCode = '';
-    for(var i=1; i<=num; i++) {
-      tmpCode += document.getElementById(i).value;
+  const handleChange = (element, index)=> {
+    if (isNaN(element.value)) return;
+    const newOtp = [...otp];
+    newOtp[index] = element.value;
+    setOtp(newOtp);
+    if (element.nextSibling && element.value) {
+      element.nextSibling.focus();
     }
-    setCode({code: Number(tmpCode)})
   }
 
-  const moveFocus = (event) => {
-    if(!event) {return;}
-    let id = Number(event.target.id);
-    if(event.key !== 'Backspace') {
-      if(document.getElementById(id).nextElementSibling) {
-        document.getElementById(id).nextElementSibling.focus();
-      }
-    } else if(event.key === 'Backspace') {
-      if(document.getElementById(id).previousElementSibling) {
-        document.getElementById(id).previousElementSibling.focus();
-        return;
-      }
+  const handleKeyDown = (event, index) => {
+    if (event.key === 'Backspace' && !otp[index] && index > 0) {
+      event.target.previousSibling.focus();
     }
+  }
+
+  const handleVerify = () => {
+    let code = '';
+    code = otp.reduce((code, num) => {
+      return code + num;
+    });
+    props.verify(parseInt(code));
   }
 
   return (
     <div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">{props.msg ? props.msg : 'Enter the verification code'}</label> <br />
-            <div className="otp-container" onChange={onChange}>
-              <input type="text" id='1' style={{width: window.innerHeight < 700 ? '40px' : '30px', height: window.innerHeight < 700 ? '40px' : '30px'}} className="otp-box" maxLength="1" autoComplete='off' onKeyUp={moveFocus} />
-              <input type="text" id='2' style={{width: window.innerHeight < 700 ? '40px' : '30px', height: window.innerHeight < 700 ? '40px' : '30px'}} className="otp-box" maxLength="1" autoComplete='off' onKeyUp={moveFocus} />
-              <input type="text" id='3' style={{width: window.innerHeight < 700 ? '40px' : '30px', height: window.innerHeight < 700 ? '40px' : '30px'}} className="otp-box" maxLength="1" autoComplete='off' onKeyUp={moveFocus} />
-              <input type="text" id='4' style={{width: window.innerHeight < 700 ? '40px' : '30px', height: window.innerHeight < 700 ? '40px' : '30px'}} className="otp-box" maxLength="1" autoComplete='off' onKeyUp={moveFocus} />
-              <input type="text" id='5' style={{width: window.innerHeight < 700 ? '40px' : '30px', height: window.innerHeight < 700 ? '40px' : '30px'}} className="otp-box" maxLength="1" autoComplete='off' onKeyUp={moveFocus} />
-              <input type="text" id='6' style={{width: window.innerHeight < 700 ? '40px' : '30px', height: window.innerHeight < 700 ? '40px' : '30px'}} className="otp-box" maxLength="1" autoComplete='off' onKeyUp={moveFocus} />
-            </div>
-          <div id="emailHelp" className="form-text bg-danger p-1 text-center border rounded text-white">***Please check in spam folder too</div>
-        </div>
-        <div className="buttons d-flex my-3">
-          <button type="button" onClick={props.sendEmail} className="btn btn-warning mx-2">Resend <i className="fa-solid fa-paper-plane mx-2"></i></button>
-          <button type="button" onClick={() => {props.verify(code.code)}} className="btn btn-success mx-2">Verify <i className="fa-solid fa-certificate mx-2"></i></button>
-        </div>
+      <div style={{ display: 'flex', gap: '10px' }}>
+        {otp.map((value, index) => (
+          <input
+            key={index}
+            type="text"
+            maxLength="1"
+            value={value}
+            onChange={(e) => handleChange(e.target, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+            style={{
+              width: '40px',
+              height: '40px',
+              textAlign: 'center',
+              fontSize: '20px',
+              borderRadius: '5px',
+              border: '1px solid #ccc',
+            }}
+          />
+        ))}
+      </div>
+      <div className="buttons d-flex my-3">
+        <button type="button" onClick={props.sendEmail} className="btn btn-warning mx-2">Resend <i className="fa-solid fa-paper-plane mx-2"></i></button>
+        <button type="button" onClick={handleVerify} className="btn btn-success mx-2">Verify <i className="fa-solid fa-certificate mx-2"></i></button>
+      </div>
     </div>
   )
 }
