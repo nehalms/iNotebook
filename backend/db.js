478053aa052ebe059f3dfa588c5fbad8a3584c65
nehalms <nehalms3388@gmail.com>
2025-10-26 13:40:29 +0530

    mongo pooled connections

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-// const mongoURI = "mongodb://127.0.0.1/inotebook" //inotebook is database name
-const connectToMongo = ()=> {
-    try {
-        mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ifjjiuu.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0`).then(console.log("Connected to Mongodb"));
-    } catch (err) {
-        console.log(err);
-    }
-}
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ifjjiuu.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0`;
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      maxPoolSize: 10,
+      minPoolSize: 3,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+    });
+    console.log("Connected to MongoDB (pooled connection)");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+  }
+};
+
 
 module.exports = connectToMongo;
