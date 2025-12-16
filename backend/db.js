e7acec9aa2514b575284b081fa0b3f46fd98bf1f
nehalms <nehalms3388@gmail.com>
2025-12-16 10:44:35 +0530

    db conn chngs

--- backend/db.js
+++ backend/db.js
@@ -2,19 +2,40 @@ const mongoose = require("mongoose");
 
 const MONGO_URI = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ifjjiuu.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0`;
 
-const connectToMongo = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-      maxPoolSize: 20,
-      minPoolSize: 5,
-      serverSelectionTimeoutMS: 20000,
-      socketTimeoutMS: 45000,
-    });
-    console.log("Connected to MongoDB (pooled connection)");
-  } catch (err) {
-    console.error("MongoDB connection error:", err.message);
-  }
-};
+let cached = global.mongoose;
 
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectToMongo = async () => {
+  if (cached.conn) {
+    console.log("Using existing MongoDB connection");
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      maxPoolSize: 1,
+      minPoolSize: 1,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+    };
+
+    console.log("Creating new MongoDB connection...");
+    cached.promise = mongoose.connect(MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+};
 
 module.exports = connectToMongo;
